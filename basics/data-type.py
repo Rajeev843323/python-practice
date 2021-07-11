7deaee5250ce8a7c2ef42eafe33cfe1871dd4b23
Rajeev Kumar (JSL-Domestic Sales) <kumar.rajeev@jindalsteel.com>
2021-07-11 23:04:47 +0530

    Committer: Rajeev Kumar (JSL-Domestic Sales) <kumar.rajeev@jindalsteel.com>
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	new file:   basics/data-type.py

---
+++ basics/data-type.py
@@ -0,0 +1,27 @@
+'''There are 6 data types in python
+1)Number (Integer, Float)
+2)String
+3)Set
+4)List
+5)Tuple
+6)Dictionary
+
+Number, String and Tuples are imutable it means its value can not be changed
+however List, Set and Dictionary are mutable it means its value can be change
+for example'''
+
+#Example of Integer data Type, no need to be write integer or int while defining integer type
+a=10
+b=20
+c=a+b
+print(c)
+
+#Example of float data type
+a=10.5
+b=20.1
+print(a+b)
+
+#Example of String data type
+a='Rajeev'
+b='Kumar'
+print(a+' '+b)
