c809d215ad5c648923e5a90f5188f809693666ba
Rajeev Kumar (JSL-Domestic Sales) <kumar.rajeev@jindalsteel.com>
2021-07-11 18:58:00 +0530

    python comments Committer: Rajeev Kumar (JSL-Domestic Sales) <kumar.rajeev@jindalsteel.com> On branch main Your branch is up to date with 'origin/main'. Changes to be committed: 	new file:   basics/comment.py

---
+++ basics/comment.py
@@ -0,0 +1,9 @@
+# This is a single line comment. it starts with hash (#)
+
+
+'''This is a multiline comment.
+it starts and ends with triple quotes character'''
+
+
+"""This is another multiline comment. 
+it starts and end with 3 time double quote characher"""
