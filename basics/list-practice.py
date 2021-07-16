162344dc1a7be1125f1910a997e811aceb0573c0
Rajeev Kumar (JSL-Domestic Sales) <kumar.rajeev@jindalsteel.com>
2021-07-16 17:27:08 +0530

    List practice have changes in range
    Committer: Rajeev Kumar (JSL-Domestic Sales) <kumar.rajeev@jindalsteel.com>
    
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   basics/list-practice.py

--- basics/list-practice.py
+++ basics/list-practice.py
@@ -41,4 +41,37 @@ Dictionary: ordered, changable, no duplicate"""
 thislist = ["apple", "banana", "cherry"]
 print(thislist[1])
 
-#Negative index: -1 index indicating the last item in the list
+#Negative index: -1 index indicating the last item and -2 means second last item in the list
+#print last item from the list
+thislist=['apple','banana','cherry','mango']
+print(thislist[-1])
+print(thislist[-2])
+
+#Range of indexes
+#Print 3rd, 4th, 5th item from list
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:5])
+#Here output start with index 2 (included) and end at index 5 (not included)
+
+#Print begining to index 4 (not included kiwi)
+print(thislist[:4])
+
+#Print index 2 to end
+print(thislist[2:])
+
+#Range of negative indexs
+#Print -4 (orange) to -1 (mango not included)
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[-4:-1])
+
+#Check item in the list
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+if 'orange' in thislist:
+    print('yes orange available in list')
+else:
+    print('not available')    
+
+#count items in list
+thislist = ["apple", "banana", "cherry", 'banana',"orange", "kiwi", 'mango',"melon", "mango",'guava']
+print(thislist.count('banana'))
+print(len(thislist))
